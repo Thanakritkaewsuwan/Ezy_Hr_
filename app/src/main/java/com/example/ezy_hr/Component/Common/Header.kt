e106adc820fbf9209dae4dbe4964f30f30b4440b
MAP-PC-062\thanakrit <thanakrit@micware-ap.com>
2025-06-23 12:59:08 +0700

    add string xml value

--- app/src/main/java/com/example/ezy_hr/Component/Common/Header.kt
+++ app/src/main/java/com/example/ezy_hr/Component/Common/Header.kt
@@ -1,2 +1,73 @@
-package com.example.ezy_hr.Component.Common
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.createFontFamilyResolver
+
+
+@Composable
+fun Header(title: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF1FB5FF))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically)
+        {
+            Text(
+                text = "Back",
+                style = MaterialTheme.typography.headlineMedium,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineMedium,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun Header2(title: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF1FB5FF))
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row()
+        {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+    MaterialTheme {
+        Header2(
+            title = "Home",
+        )
+    }
+}
